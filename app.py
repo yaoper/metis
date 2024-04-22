7dc77344a72a3f1e1ae2b8a509b7834ed8d51983
yaoper <yaoper@163.com>
2024-04-22 17:54:33 +0800

    debug using zhipu ai llm

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import streamlit as st
-from langchain_openai import ChatOpenAI
 import os
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 import sys
-sys.path.append("./database") # 将父目录放入系统路径中
+
+sys.path.append("./database")  # 将父目录放入系统路径中
 from embedding import ZhipuAIEmbeddings
 from llm import ZhipuAILLM
 from langchain.vectorstores.chroma import Chroma
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())    # read local .env file
+
+_ = load_dotenv(find_dotenv())  # read local .env file
 
 zhipuai_api_key = os.environ['ZHIPUAI_API_KEY']
+
 
 def generate_response(input_text, openai_api_key):
     llm = ZhipuAILLM(temperature=0.7, openai_api_key=zhipuai_api_key)
     output = llm.invoke(input_text)
     output_parser = StrOutputParser()
     output = output_parser.invoke(output)
-    #st.info(output)
     return output
 
 
@@ -37,8 +38,9 @@ def get_vectordb():
     )
     return vectordb
 
-#带有历史记录的问答链
-def get_chat_qa_chain(question:str, openai_api_key:str):
+
+# 带有历史记录的问答链
+def get_chat_qa_chain(question: str, openai_api_key: str):
     vectordb = get_vectordb()
     llm = ZhipuAILLM(temperature=0.7, openai_api_key=zhipuai_api_key)
     # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=zhipuai_api_key)
@@ -55,37 +57,40 @@ def get_chat_qa_chain(question:str, openai_api_key:str):
     result = qa({"question": question})
     return result['answer']
 
-#不带历史记录的问答链
-def get_qa_chain(question:str, openai_api_key:str):
+
+# 不带历史记录的问答链
+def get_qa_chain(question: str, openai_api_key: str):
+    # 获取向量数据库
     vectordb = get_vectordb()
     llm = ZhipuAILLM(temperature=0.7, openai_api_key=zhipuai_api_key)
+
     # llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=zhipuai_api_key)
     template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
         案。最多使用三句话。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
         {context}
         问题: {question}
         """
-    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+    qa_chain_prompt = PromptTemplate(input_variables=["context", "question"], template=template)
     qa_chain = RetrievalQA.from_chain_type(
-                                    llm,
-                                   retriever=vectordb.as_retriever(),
-                                   return_source_documents=True,
-                                   chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
-    result = qa_chain({"query": question})
+        llm,
+        retriever=vectordb.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": qa_chain_prompt})
+    result = qa_chain({"query": question, "context": "你是一个资深旅行爱好者，知道中国各地的名胜古迹，假如颐和园在南京也有一个园区，你会回答我关于一些景点的问题。"})
     return result["result"]
 
 
 # Streamlit 应用程序界面
 def main():
-    st.title('🦜🔗 动手学大模型应用开发')
+    st.title('🦜🔗 LangChain 大模型应用开发')
     openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
     # 添加一个选择按钮来选择不同的模型
-    #selected_method = st.sidebar.selectbox("选择模式", ["qa_chain", "chat_qa_chain", "None"])
     selected_method = st.radio(
         "你想选择哪种模式进行对话？",
         ["None", "qa_chain", "chat_qa_chain"],
-        captions = ["不使用检索问答的普通模式", "不带历史记录的检索问答模式", "带历史记录的检索问答模式"])
+        captions=["不使用检索问答的普通模式", "不带历史记录的检索问答模式", "带历史记录的检索问答模式"]
+    )
 
     # 用于跟踪对话历史
     if 'messages' not in st.session_state:
@@ -96,6 +101,7 @@ def main():
         # 将用户输入添加到对话历史中
         st.session_state.messages.append({"role": "user", "text": prompt})
 
+        answer = None
         if selected_method == "None":
             # 调用 respond 函数获取回答
             answer = generate_response(prompt, openai_api_key)
